af0da32a28efb69e78d6af412e25c0249155802d
aravind2006 <67790166+aravind2006@users.noreply.github.com>
2022-05-24 14:08:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbHNJGzTd-NYB45WMfNpkfw5RZrLYYvfw",
+  authDomain: "storytelling-app-f3da8.firebaseapp.com",
+  projectId: "storytelling-app-f3da8",
+  storageBucket: "storytelling-app-f3da8.appspot.com",
+  messagingSenderId: "47643534127",
+  appId: "1:47643534127:web:d1205851741031a26e37da"
+  };
